a37f9a6ec29cc99c60c95de212753a3d8b8b1745
abimbolaalabi <ibrahimade92@gmail.com>
2026-04-27 13:42:04 +0100

    routes and working with json

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
-const express = require('express')
+// const express = require('express')
 
+
+// const app = express();
+
+// app.listen( 3000, ()=> {
+//     console.log("server is running")
+// })
 
 const app = express();
 
-app.listen( 3000, ()=> {
-    console.log("server is running")
+//body parsing
+app.use(express.json())
+
+const studentInfo = [
+  { name: "Alice", age: 20, grade: "A" },
+  { name: "Bob", age: 21, grade: "B" },
+  { name: "Charlie", age: 22, grade: "C" }
+]
+
+app.get("/all-data",(req , res)=>{
+  res.json(studentInfo[0])
 })
+
+// Step 3: Define a route handler for GET requests to /
+app.get('/', function(req, res) {
+  res.send('Hello, World! Welcome to Express.js!');
+});
+
+app.get("/about" , (request , response)=>{
+  response.send("this is about page")
+})
+
+app.get("/contact" , (request , response)=>{
+  response.send("this is contact page")
+})
+
+// Step 4: Start listening on port 3000
+app.listen(3000, function() {
+  console.log('Server is up and running');
+});
